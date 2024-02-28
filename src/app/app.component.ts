2a7e54b3b56b276f276ef68f8c66298f4efbea01
Patrick Ranval <pat.ranval@gmail.com>
2024-02-27 18:26:49 -0600

    Procrastinated by adding Navbar
    Probably should actually make http service....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TrayViewComponent } from './tray-view/tray-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TrayViewComponent],
+  imports: [RouterOutlet, TrayViewComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
